53b4b3dd0653f1f58baeb9ad7076d7ab2cb4714e
Kazuhiro Kubota <k2.wanko@gmail.com>
2018-09-23 03:46:30 +0900

    Googleログインの実装

--- public/main.js
+++ public/main.js
@@ -3,22 +3,50 @@ async function initAuth() {
 
     const $app = document.getElementById('app');
     const $load = document.getElementById('load');
+    const $authButton = document.getElementById('auth-button');
 
     let loaded = false;
+    let loginUser = null;
     auth.onAuthStateChanged(user => {
         if (!loaded) {
             loaded = true;
             $load.style.display = 'none';
             $app.style.display = '';
         }
-        console.log('@@@', user);
+
+        loginUser = user;
+
+        if (user) {
+            $authButton.innerText = 'Logout';
+        } else {
+            $authButton.innerText = 'Login';
+        }
     });
+
+    $authButton.addEventListener('click', () => {
+        if(loginUser) {
+            auth.signOut();
+            return;
+        }
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithRedirect(provider);
+    });
+
+    try {
+        const result = await auth.getRedirectResult();
+        // 次のトークンを使ってGoogle APIを呼び出せる。
+        // const token = result.credential.accessToken;
+    } catch (err) {
+        alert(err.message);
+        console.error(err);
+    }
+
 };
 
 async function main() {
     await initAuth()
 };
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     main().catch(err => console.error(err));
 });
