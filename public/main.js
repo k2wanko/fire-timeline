f5de151dd4cec12e3890c7d2485c3d9d051cc85a
Kazuhiro Kubota <k2.wanko@gmail.com>
2018-09-24 17:06:00 +0900

    ユーザーデータをFirestoreに保存する

--- public/main.js
+++ public/main.js
@@ -1,9 +1,20 @@
+function showError(err) {
+    alert(err.message);
+    console.error(err);
+};
+
 async function initAuth() {
     const auth = firebase.auth();
+    const db = firebase.firestore();
+    db.settings({ timestampsInSnapshots: true });
 
     const $app = document.getElementById('app');
     const $load = document.getElementById('load');
     const $authButton = document.getElementById('auth-button');
+    const $appTl = document.getElementById('app-tl');
+    const $uid = document.getElementById('uid');
+    const $profileName = document.getElementById('profile-name');
+    const $profileImage = document.getElementById('profile-image');
 
     let loaded = false;
     let loginUser = null;
@@ -18,13 +29,26 @@ async function initAuth() {
 
         if (user) {
             $authButton.innerText = 'Logout';
+            $appTl.style.display = 'flex'; // アプリケーションを表示する
+
+            // ログイン中のユーザー情報を表示する
+            $uid.innerText = user.uid;
+            $profileName.innerText = user.displayName;
+            $profileImage.src = user.photoURL;
+
+            // ログイン中のユーザーのプロフィールを保存する
+            db.collection('users').doc(user.uid).set({
+                name: user.displayName,
+                photoURL: user.photoURL,
+            }).catch(showError);
         } else {
+            $appTl.style.display = 'none';
             $authButton.innerText = 'Login';
         }
     });
 
     $authButton.addEventListener('click', () => {
-        if(loginUser) {
+        if (loginUser) {
             auth.signOut();
             return;
         }
@@ -33,12 +57,9 @@ async function initAuth() {
     });
 
     try {
-        const result = await auth.getRedirectResult();
-        // 次のトークンを使ってGoogle APIを呼び出せる。
-        // const token = result.credential.accessToken;
+        await auth.getRedirectResult();
     } catch (err) {
-        alert(err.message);
-        console.error(err);
+        showError(err);
     }
 
 };
